f175ddd006496e2504e7f51dfedc49b00b18578b
Pras Pitasawad <prasrodo@gmail.com>
2023-02-23 14:38:35 +0700

    add authentication

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -2,7 +2,9 @@ const express = require('express')
 const {getHospitals, getHospital, createHospitals, updateHospital, deleteHospital} = require('../controller/hospitals');
 const router = express.Router();
 
-router.route('/').get(getHospitals).post(createHospitals);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+const {protect,authorize} =require('../middleware/auth');
+
+router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospitals);
+router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'),deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db')
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
 
 //load env vars 
 dotenv.config({path:'./config/config.env'});
@@ -13,8 +14,14 @@ const app=express();
 //add body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth)
 
 const PORT=process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
