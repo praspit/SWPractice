770697a48326a039fc562367effab1c7c0596929
Pras Pitasawad <prasrodo@gmail.com>
2023-03-23 20:26:36 +0700

    add VacCenter API

--- controller/hospitals.js
+++ controller/hospitals.js
@@ -1,4 +1,5 @@
 const Hospital = require('../models/Hospital')
+const vacCenter = require('../models/VacCenter')
 //@desc     Get all hospitals
 //@routes   GET /api/v1/hospitals
 //@access   Public
@@ -121,4 +122,19 @@ exports.deleteHospital= async (req,res,next)=>{
     } catch(err) {
         res.status(400).json({success:false});
     }
+}
+
+//@desc     GET vaccine centers
+//@route    GET /api/v1/hospitals/vacCenters/
+//@access   Public
+exports.getVacCenters = (req,res,next)=>{
+    vacCenter.getAll((err,data) => {
+        if(err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving Vaccine Centers."
+            });
+        } else {
+            res.send(data);
+        }
+    });
 }
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getHospitals, getHospital, createHospitals, updateHospital, deleteHospital} = require('../controller/hospitals');
+const {getHospitals, getHospital, createHospitals, updateHospital, deleteHospital, getVacCenters} = require('../controller/hospitals');
 const router = express.Router();
 
 //Include other resource routers
@@ -9,6 +9,7 @@ const {protect,authorize} =require('../middleware/auth');
 
 //Re-route into other resource routers
 router.use('/:hospitalId/appointments/',appointmentRouter);
+router.route('/vacCenters').get(getVacCenters);
 router.route('/').get(getHospitals).post(protect, authorize('admin'), createHospitals);
 router.route('/:id').get(getHospital).put(protect, authorize('admin'), updateHospital).delete(protect, authorize('admin'),deleteHospital);
 
